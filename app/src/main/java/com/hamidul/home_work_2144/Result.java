6813a2be2c3fc459044c161f8b982e919c6c3a73
user <practicecoddin@gmail.com>
2023-08-01 22:38:20 +0600

    Home_Work_214.4

---
+++ app/src/main/java/com/hamidul/home_work_2144/Result.java
@@ -0,0 +1,14 @@
+package com.hamidul.home_work_2144;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
