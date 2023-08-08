4324ccc70ac0c10e62a080847cfbd21852f180df
user <practicecoddin@gmail.com>
2023-08-08 07:42:17 +0600

    Home_Work_214.5

--- app/src/main/java/com/hamidul/home_work_2145/MainActivity.java
+++ app/src/main/java/com/hamidul/home_work_2145/MainActivity.java
@@ -3,8 +3,17 @@ package com.hamidul.home_work_2145;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Home_Work_214.5
+
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        //======================================================================================================================
+
+
+
+
 
     }
 }
